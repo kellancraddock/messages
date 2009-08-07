984a4c707923ecfe7bbd465bfdea06c272656b55
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-06 20:59:54 -0700

    added index controller and view

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+	class IndexController extends Zend_Controller_Action
+	{
+		public function indexAction()
+		{
+			
+		}
+		
+		public function addAuthorAction()
+		{
+			$this->view->alert = "Author Added!";
+		}
+		
+		public function addMessageAction()
+		{
+		
+		}
+		
+		public function voteUpAction()
+		{
+		
+		}
+	}
+	
+?>
