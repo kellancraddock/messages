a8e81aa5d029a51a5033b65d51e2ab01e1b96ca8
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-07 00:12:36 -0700

    added actions for index controller

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,24 +1,56 @@
 <?php
+	require_once('../app/models/AuthorsModel.php');
+	require_once('../app/models/MessageModel.php');
+	
 	class IndexController extends Zend_Controller_Action
 	{
+		public function init() {
+			$this->AuthorsModel = new AuthorsModel();
+			$this->MessageModel = new MessageModel();
+			
+			$this->view->authors = $this->AuthorsModel->getAuthors();
+			$this->view->messages = $this->MessageModel->getMessages();
+		}
+		
 		public function indexAction()
 		{
+
+		}
+		
+		public function addauthorAction()
+		{
+			$arguments = array($_POST['first_name'], $_POST['last_name']);
 			
+			$this->AuthorsModel->addAuthor($arguments);
+			
+			$this->view->authors = $this->AuthorsModel->getAuthors();
+			
+			$this->view->alert = var_dump($response);
 		}
 		
-		public function addAuthorAction()
+		public function addmessageAction()
 		{
-			$this->view->alert = "Author Added!";
+			$arguments = array($_POST['message'], $_POST['author']);
+			
+			$this->MessageModel->addMessage($arguments);
+			
+			$this->view->messages = $this->MessageModel->getMessages();
+
 		}
 		
-		public function addMessageAction()
+		public function deletemessageAction()
 		{
-		
+			$arguments = array($this->_request->getParam('id'));
+			
+			$this->MessageModel->deleteMessage($arguments);
+			
+			$this->view->messages = $this->MessageModel->getMessages();
+
 		}
 		
-		public function voteUpAction()
+		public function voteupAction()
 		{
-		
+			//pass in defalut info (messages etc)
 		}
 	}
 	
