4a49308fe96ff7b63e675ad1cbf481278770e399
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-07 18:53:58 -0700

    Moved actions from index controller

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,52 +5,16 @@
 	class IndexController extends Zend_Controller_Action
 	{
 		public function init() {
+			$this->session_alert = new Zend_Session_Namespace('alert');
 			$this->AuthorsModel = new AuthorsModel();
 			$this->MessageModel = new MessageModel();
-			
-			$this->view->authors = $this->AuthorsModel->getAuthors();
-			$this->view->messages = $this->MessageModel->getMessages();
 		}
 		
 		public function indexAction()
 		{
-
-		}
-		
-		public function addauthorAction()
-		{
-			$arguments = array($_POST['first_name'], $_POST['last_name']);
-			
-			$this->AuthorsModel->addAuthor($arguments);
-			
 			$this->view->authors = $this->AuthorsModel->getAuthors();
-			
-			$this->view->alert = var_dump($response);
-		}
-		
-		public function addmessageAction()
-		{
-			$arguments = array($_POST['message'], $_POST['author']);
-			
-			$this->MessageModel->addMessage($arguments);
-			
 			$this->view->messages = $this->MessageModel->getMessages();
-
-		}
-		
-		public function deletemessageAction()
-		{
-			$arguments = array($this->_request->getParam('id'));
-			
-			$this->MessageModel->deleteMessage($arguments);
-			
-			$this->view->messages = $this->MessageModel->getMessages();
-
-		}
-		
-		public function voteupAction()
-		{
-			//pass in defalut info (messages etc)
+			$this->view->alert = $this->session_alert;
 		}
 	}
 	
